f653e28e4eb3f520c2205c752a65b804d941129a
Anonda25 <anondabiswas25@gmail.com>
2024-11-12 11:28:57 +0600

    added the condition

--- src/Components/SignUp/Sign_Up.jsx
+++ src/Components/SignUp/Sign_Up.jsx
@@ -1,17 +1,45 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebase_init";
-
+import { IoEyeOutline } from "react-icons/io5";
+import { GoEyeClosed } from "react-icons/go";
 function Sign_Up() {
   const [errormessage, setErrorMessage] = useState("");
+  const [success, setSuccess]=useState(false)
+  const [showPassword, setShowPassword]= useState(false)
+
+
+
+
+
   const hendelSignUp = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
+    const trems = e.target.trems.checked
     setErrorMessage('')
+     setSuccess(false);
+
+     if (!trems){
+      setErrorMessage('accept our rual an condition')
+      return;
+     }
+       if (password.length < 6) {
+         setErrorMessage("please type 6 careact");
+         return;
+       }
+      const isValidPassword =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+      
+           if (!isValidPassword.test(password)) {
+             setErrorMessage("please type one upercase and one lowercase and one splical chareact");
+             return;
+           }
     createUserWithEmailAndPassword(auth, email, password)
       .then((res) => {
         console.log(res.user);
+        setSuccess(true)
       })
       .catch((error) => {
         console.log("error", error.message);
@@ -34,31 +62,49 @@ function Sign_Up() {
             required
           />
         </div>
-        <div className="form-control">
+        <div className="form-control relative">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="password"
             name="password"
             className="input input-bordered"
             required
           />
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className="btn btn-xs absolute right-2 top-12"
+          >
+            {showPassword ? <GoEyeClosed /> : <IoEyeOutline />}
+          </button>
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">
               Forgot password?
             </a>
           </label>
         </div>
+        <div className="form-control">
+          <label className="cursor-pointer label justify-start gap-3">
+            <input
+              type="checkbox"
+              defaultChecked
+              name="trems"
+              className="checkbox checkbox-success"
+            />
+            <span className="label-text">Accepct our Teams and Condition</span>
+          </label>
+        </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary">sign Up</button>
         </div>
         {errormessage && (
           <div>
             <p className="text-xl text-red-400">{errormessage}</p>
           </div>
         )}
+        {success && <p className="text-green-700">sign up successfily</p>}
       </form>
     </div>
   );
